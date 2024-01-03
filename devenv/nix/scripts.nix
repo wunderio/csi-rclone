2ae1d2fd14b325cd25c5dfb9ad479d2d0d89cb16
Ralf Grubenmann <ralf.grubenmann@gmail.com>
2024-01-03 15:21:31 +0100

    add chart publish action

--- devenv/nix/scripts.nix
+++ devenv/nix/scripts.nix
@@ -47,7 +47,7 @@ let
        kind load docker-image csi-rclone:latest  --name csi-rclone-k8s
   
        echo "Render helm chart with new container version"
-       helm template -n csi-rclone csi-rclone deploy/chart > devenv/kind/deploy-kind/csi-rclone-templated-chart.yaml
+       helm template -n csi-rclone csi-rclone deploy/csi-rclone > devenv/kind/deploy-kind/csi-rclone-templated-chart.yaml
   
        # TODO: use tee
   
